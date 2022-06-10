de6f062f52d7c56b43cdc0a3989c9ac93e4b50be
kimwong9999 <103122457+kimwong9999@users.noreply.github.com>
2022-06-10 09:54:58 +0100

    Commit of MovieInput.java

--- src/main/java/com/qa/cinema/input/MovieInput.java
+++ src/main/java/com/qa/cinema/input/MovieInput.java
@@ -1,1 +1,21 @@
+package com.qa.cinema.input;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@CrossOrigin
+public class MovieInput 
+{
+	private int id;
+	private String title;
+	private String article;
+	private int release;
+	private int duration;
+	private String chroma;
+	private String ratio;
+	private Integer[] cast;
+}
